9017cad2a807fb243def066f17128c5c2a04c32d
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2025-08-25 18:28:15 +0100

    resolution de liaison exe avec app render

--- run_desktop.py
+++ run_desktop.py
@@ -1,13 +1,5 @@
 import webview # type: ignore
-import os
-import threading
 
-def start_django():
-    os.system("python manage.py runserver")
-
-
-# Lancer Django dans un thread séparé
-threading.Thread(target=start_django, daemon=True).start()
 
 # Attendre que le serveur soit prêt (optionnel : ajouter un délai ou un ping)
 webview.create_window("Pressing Trois-O", "https://trois-o-services.onrender.com/", width=1200, height=800)
