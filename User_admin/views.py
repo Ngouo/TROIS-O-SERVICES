2bbb0a35b94f2005da240be836cc2524cf18e47c
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2025-08-26 15:44:17 +0100

    utilisation de csrf_except

--- User_admin/views.py
+++ User_admin/views.py
@@ -3,7 +3,10 @@
 # Create your views here.
 from django.contrib.auth.views import LoginView
 from .forms import BootstrapLoginForm
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
+@method_decorator(csrf_exempt, name='dispatch')
 class MonLoginView(LoginView):
     template_name = 'User_admin/login.html'
     authentication_form = BootstrapLoginForm
